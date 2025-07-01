28a1de644b51aa3d9759eab55864fc2178bcd81c
foxsmarty <nooks@foxmail.com>
2025-07-01 13:12:25 +0800

    🎨 Chore:  Minor adjustments

--- buddy/src/ui/components/home/PluginPageGrid.vue
+++ buddy/src/ui/components/home/PluginPageGrid.vue
@@ -21,7 +21,7 @@ onUnmounted(() => {
 <template>
     <div class="w-full h-full flex flex-col overflow-hidden bg-amber-100/0">
         <div class="flex-1">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-12">
                 <div v-for="(plugin, index) in plugins" :key="index">
                     <PluginPage :plugin="plugin" />
                 </div>
